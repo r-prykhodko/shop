c539b662e35c85e9277db72393b559149cc61d16
Roman Prykhodko <r.prykhodko@yahoo.com>
2024-01-27 21:00:19 +0200

    111

--- mysite/goods/models.py
+++ mysite/goods/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
+
+    class Meta:
+        db_table = 'category'
