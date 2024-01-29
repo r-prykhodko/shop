fcb298c47de4f69562da919b5786fbd56e0b1263
Roman Prykhodko <r.prykhodko@yahoo.com>
2024-01-29 20:09:16 +0200

    q

--- mysite/goods/urls.py
+++ mysite/goods/urls.py
@@ -24,6 +24,6 @@ app_name = 'goods'
 urlpatterns = [
   
     path('', views.catalog, name='index'),
-    path('product/', views.product, name='product'),
+    path('product/<int:product_id>', views.product, name='product'),
 
 ]
--- mysite/goods/views.py
+++ mysite/goods/views.py
@@ -14,5 +14,11 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_id):
+    product = Products.objects.get(id=product_id)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, "goods/product.html", context=context)
