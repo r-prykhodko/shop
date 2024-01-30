0fc75458cd16b6eb0137d9a65ed7c047cebdb615
Roman Prykhodko <r.prykhodko@yahoo.com>
2024-01-30 20:06:15 +0200

    1

--- mysite/goods/urls.py
+++ mysite/goods/urls.py
@@ -23,7 +23,9 @@ app_name = 'goods'
 
 urlpatterns = [
   
-    path('', views.catalog, name='index'),
-    path('product/<int:product_id>', views.product, name='product'),
+    path('<slug:category_slug>/', views.catalog, name='index'),
+    path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
+    path('product/<slug:product_slug>', views.product, name='product'),
+    # path('product/<int:product_id>', views.product, name='product'),
 
 ]
--- mysite/goods/views.py
+++ mysite/goods/views.py
@@ -1,21 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
+from django.core.paginator import Paginator
 
 from goods.models import Products
 
 
-def catalog(request):
-
-    goods = Products.objects.all()
+def catalog(request, category_slug, page=1):
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+    
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
 
     context = {
         "title": "Home - catalog",
-        "goods": goods
+        "goods": current_page,
+        "slug_url": category_slug
     }
     return render(request, "goods/catalog.html", context)
 
 
-def product(request, product_id):
-    product = Products.objects.get(id=product_id)
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
 
     context = {
         'product': product
